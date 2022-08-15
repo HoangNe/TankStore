cd1cceed29383168ca0b6d894f8ba38550f0c90c
HoangNe <hoangsoft2804@gmail.com>
2022-08-15 09:34:17 +0700

    pending1

--- TankShop/src/app/app-routing.module.ts
+++ TankShop/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'', component:HomeComponent},
+    {path: 'home', component:HomeComponent},
+    {path: 'login', component:LoginComponent},
+    {path: '**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TankShop/src/app/app.module.ts
+++ TankShop/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    LoginComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
